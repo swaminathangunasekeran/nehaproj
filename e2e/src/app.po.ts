e14fcea2bb4c913e88a30363b56941354f6f4da3
swaminathangunasekaran <swaminathan8223@gmail.com>
2021-01-19 22:38:36 +0530

    intial setup for UI

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,13 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element } from "protractor";
 
 export class AppPage {
-  async navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl);
+  navigateTo() {
+    return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  async getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText();
+  getTitleText() {
+    return element(by.css("app-root .content span")).getText() as Promise<
+      string
+    >;
   }
 }
